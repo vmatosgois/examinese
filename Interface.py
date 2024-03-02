ca514b24ddeb6d9ddfdbf7c30dbd7ed9883df244
Victor Matos <victormatosgois@hotmail.com>
2024-03-02 17:02:21 -0300

    Acrescenta aba Lab

--- Interface.py
+++ Interface.py
@@ -9,7 +9,6 @@ from PIL import Image
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -76,64 +75,73 @@ class App(customtkinter.CTk):
         
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Tema:", anchor="w")
         self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Claro", "Escuro", "Sistema"],
+        self.appearance_mode_optionmenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Claro", "Escuro", "Sistema"],
                                                                        command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionmenu.grid(row=8, column=0, padx=20, pady=(10, 10))
         
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="Proporção:", anchor="w")
         self.scaling_label.grid(row=9, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+        self.scaling_optionmenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=10, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionmenu.grid(row=10, column=0, padx=20, pady=(10, 20))
         
         # Home
         
-        self.homeframe = customtkinter.CTkFrame(self, corner_radius= 0, fg_color='transparent')
-        self.homeframe.grid(row=0, column= 1, rowspan=5, columnspan= 5 ,sticky= 'nsew')
-        self.homeframe.grid_columnconfigure(1, weight=1)
-        self.homeframe.grid_rowconfigure((0, 1), weight=1)
+        # self.homeframe = customtkinter.CTkFrame(self, corner_radius= 0, fg_color='transparent')
+        # self.homeframe.grid(row=0, column= 1, rowspan=5, columnspan= 5 ,sticky= 'nsew')
+        # self.homeframe.grid_columnconfigure(1, weight=1)
+        # self.homeframe.grid_rowconfigure((0, 1), weight=1)
 
-        # Tela inicial
+        # self.apresentacao = customtkinter.CTkLabel(self.homeframe, text= 'Bem-vindos ao Examine-se', font=customtkinter.CTkFont(size=30, weight="bold"))
+        # self.apresentacao.grid(row= 0, column= 1, padx= 20, pady= (50, 20))
+      
+        # self.subtitulo = customtkinter.CTkLabel(self.homeframe, text= "Este é um projeto desenvolvido\n como parte do Trabalho de Conclusão de Curso \ndo Curso de Medicina de Victor Matos Gois. \n\n Para começar, por favor escolha uma das opções no menu à esquerda.", font=customtkinter.CTkFont(size=20))
+        # self.subtitulo.grid(row= 1, column=1, padx= 20, pady= 20)
+       
+        # self.ufslogo = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "ufs_horizontal_preta.png")),
+        #                           dark_image=Image.open(os.path.join(image_path, "ufs_horizontal_branca.png")), size= (275, 112))
+        # self.ufs = customtkinter.CTkLabel(self.homeframe, text='', image=self.ufslogo ,anchor='s')
+        # self.ufs.grid(row= 2, column= 1, padx=20, pady= 20)
+        
+        # Lab
+        
+#         self.labframe = customtkinter.CTkFrame(self, corner_radius= 0, fg_color='transparent')
+#         self.labframe.grid(row=0, column= 1, rowspan=5, columnspan= 5 ,sticky= 'nsew')
+#         self.labframe.grid_columnconfigure(0, weight=1)
+#         self.labframe.grid_rowconfigure((1), weight=1)
         
 
-        self.apresentacao = customtkinter.CTkLabel(self.homeframe, text= 'Bem-vindos ao Examine-se', font=customtkinter.CTkFont(size=30, weight="bold"))
-        self.apresentacao.grid(row= 0, column= 1, padx= 20, pady= (50, 20))
-        self.subtitulo = customtkinter.CTkLabel(self.homeframe, text= "Este é um projeto desenvolvido\n como parte do Trabalho de Conclusão de Curso \ndo Curso de Medicina de Victor Matos Gois. \n\n Para começar, por favor escolha uma das opções no menu à esquerda.", font=customtkinter.CTkFont(size=20))
-        self.subtitulo.grid(row= 1, column=1, padx= 20, pady= 20)
-        self.logo = customtkinter.CTkImage(light_image=Image.open("ufs_horizontal_positiva.png"),
-                                  dark_image=Image.open("ufs_horizontal_negativa.png"))
         
-        # # create main entry and button
-        # self.main_button_1 = customtkinter.CTkButton(master=self, text='Enviar', fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
-        # self.main_button_1.grid(row=3, column= 1, padx=(20, 20), pady=(20, 20), sticky="ns")
+#         self.titulo = customtkinter.CTkLabel(self.labframe,
+#                                              text= 'Formatação de exames \n Para começar, certifique-se de que os exames estão precedidos \
+# de data no formato "dia/mês/ano"\nPor exemplo: 01/01/01 ou 01/01/2001.\n\
+# Insira seus exames no campo abaixo', font=customtkinter.CTkFont(size=16, weight='bold'))
+#         self.titulo.grid(row= 0, column= 0, padx = 20, pady = (20,0), sticky='nsew')
         
-        # self.titulo = customtkinter.CTkLabel(self, text= 'Formatação de exames \n Para começar, certifique-se de que os exames estão precedidos de data no formato 00/00/00 ou 00/00/0000.', font=customtkinter.CTkFont(size=16))
-        # self.titulo.grid(row= 0, column= 1, padx = 20, pady = (20,0), sticky='nsew')
-        # self.textbox = customtkinter.CTkEntry(self,
-        #                        placeholder_text="Insira seus exames aqui",
-        #                        width=120,
-        #                        height=25,
-        #                        border_width=2,
-        #                        corner_radius=10)
-        # self.textbox.grid(row=1, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew") 
+#         self.textbox = customtkinter.CTkTextbox(self.labframe,
+#                                border_width=2,
+#                                corner_radius=10,
+#                                font=customtkinter.CTkFont(size=14))
+#         self.textbox.grid(row=1, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew") 
+
+#         self.svcopia = customtkinter.CTkCheckBox(self.labframe, text='Salvar uma cópia dos resultados')
+#         self.svcopia.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="s")
+
+#         self.concluir = customtkinter.CTkButton(self.labframe, text='Concluir', fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+#         self.concluir.grid(row=3, column= 0, padx=(20, 20), pady=(20, 20), sticky="ns")
+
+        # Espiro
+        
+        # Calculadoras
+        
+        # Ajuda
 
 
-        # # create checkbox and switch frame
-        # self.checkbox_slider_frame = customtkinter.CTkFrame(self)
-        # self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        # self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        # self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-        # self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        # self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-        # self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        # self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
-
-        # set default values
+        # Valores padrão
         self.espiro_button.configure(state="disabled")
-        # self.checkbox_3.configure(state="disabled")
-        # self.checkbox_1.select()
-        self.appearance_mode_optionemenu.set("Escuro")
-        self.scaling_optionemenu.set("100%")
+        #self.svcopia.select()
+        self.appearance_mode_optionmenu.set("Escuro")
+        self.scaling_optionmenu.set("100%")
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
--- espiro.py
+++ espiro.py
@@ -3,48 +3,8 @@ from table2ascii import table2ascii, Merge, PresetStyle
 
 demo = {'DATA': '01/12/22', 'CVF': '3.96', 'CVFPREVISTO': '3.22', 'POSCVF': '3.93', 'FEV1': '3.04',
         'FEV1PREVISTO': '2.66', 'POSFEV1': '3.54', 'FEV1/CVF PREV': '76.8%', 'FEF2575': '2.7', 'POSFEF2575': '4.14', 0: False}
-# #valores = {'CVF': '', 'CVFPREVISTO': '', 'POSCVF': '', 'FEV1': '', 'FEV1PREVISTO': '', 'POSFEV1': '', 'FEF2575': '', 'POSFEF2575': '', 0: False}
-# valores = demo
-
-
-def main():
-    global valores
-    # CVF FEV1 FEF 25-75% FEV 75
-
-    # Layout
-    sg.theme('Reddit')
-    layout = [
-        [sg.Text('Insira apenas números')],
-        [sg.Text('Data:'), sg.Input(key='DATA', size=10)],
-        [sg.Text('PRÉ CVF'), sg.Input(key='CVF', size=5), sg.Text('Previsto >'), sg.Input(
-            key='CVFPREVISTO', size=5), sg.Text('PÓS CVF'), sg.Input(key='POSCVF', size=5)],
-        [sg.Text('PRÉ FEV1'), sg.Input(key='FEV1', size=5), sg.Text('Previsto >'), sg.Input(
-            key='FEV1PREVISTO', size=5), sg.Text('PÓS FEV1'), sg.Input(key='POSFEV1', size=5)],
-        [sg.Text('Valor previsto para FEV1/CVF'),
-         sg.Input(key='FEV1/CVF PREV', size=5)],
-        [sg.Text('PRÉ FEF 25-75%'), sg.Input(key='FEF2575', size=5),
-         sg.Text('PÓS FEF 25-75%'), sg.Input(key='POSFEF2575', size=5)],
-        #[sg.Checkbox('Laudar?')],
-        [sg.Button('Salvar')],
-        [sg.Text(key='-SAÍDA-')]
-    ]
-
-    # Janela
-    window = sg.Window('LaudAí - Espirometrias', layout,
-                       element_justification='c')
-
-    # Eventos
-    while True:
-        eventos, valores = window.read()  # type: ignore
-        if eventos == sg.WINDOW_CLOSED:
-            break
-        if eventos == 'Laudar?':
-            pass
-        if eventos == 'Salvar':
-            print(valores)
-            sg.easy_print(tabela())
-            # print(tabela())
-
+valores = {'CVF': '', 'CVFPREVISTO': '', 'POSCVF': '', 'FEV1': '', 'FEV1PREVISTO': '', 'POSFEV1': '', 'FEF2575': '', 'POSFEF2575': '', 0: False}
+valores = demo
 
 def tabela():
     VF1CVFpre = round(
@@ -72,5 +32,5 @@ def tabela():
 
 
 if __name__ == '__main__':
-    main()
-    # print(tabela())
+    # main()
+    print(tabela())
