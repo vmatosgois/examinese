cf3a514c5bbd70cb7c0032290afdbf46ced9dcca
Victor Matos <victormatosgois@hotmail.com>
2024-03-25 16:21:34 -0300

    ♻️ Refatoração: Indexação de imagens

--- Interface.py
+++ Interface.py
@@ -41,28 +41,13 @@ class App(ctk.CTk):
 
         # Imagens
         
-        # image_path = "images"
-        # self.home = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "Home_preta.png")),
-        #                                          dark_image=Image.open(os.path.join(image_path, "Home_branca.png")), size=(25, 25))
-        # self.lab = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "Lab_preta.png")),
-        #                                          dark_image=Image.open(os.path.join(image_path, "Lab_branca.png")), size=(25, 25))
-        # self.espiro = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "lung_preta.png")),
-        #                                              dark_image=Image.open(os.path.join(image_path, "lung_branca.png")), size=(25, 25))
-        # self.calc = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "Calc_preta.png")),
-        #                                              dark_image=Image.open(os.path.join(image_path, "Calc_branca.png")), size=(25, 25))
-        # self.config = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "Config_preta.png")),
-        #                                              dark_image=Image.open(os.path.join(image_path, "Config_branca.png")), size=(25, 25))
-        # self.help = ctk.CTkImage(light_image=Image.open(os.path.join(image_path, "Ajuda_preta.png")),
-        #                                              dark_image=Image.open(os.path.join(image_path, "Ajuda_branca.png")), size=(25, 25))
-        
         image_path = "images"
-        self.home = ctk.CTkImage(light_image=Image.open(img.home_p_final), dark_image=Image.open(img.home_b_final), size=(25, 25))
-        self.lab = ctk.CTkImage(light_image=Image.open(img.lab_p_final), dark_image=Image.open(img.lab_b_final), size=(25, 25))
-        self.espiro = ctk.CTkImage(light_image=Image.open(img.lung_p_final), dark_image=Image.open(img.lung_b_final), size=(25, 25))
-        self.calc = ctk.CTkImage(light_image=Image.open(img.calc_p_final), dark_image=Image.open(img.calc_b_final), size=(25, 25))
-        self.config = ctk.CTkImage(light_image=Image.open(img.config_p_final), dark_image=Image.open(img.config_b_final), size=(25, 25))
-        self.help = ctk.CTkImage(light_image=Image.open(img.ajuda_p_final), dark_image=Image.open(img.ajuda_b_final), size=(25, 25))
-                        
+        self.home = ctk.CTkImage(light_image=img.home_p_final, dark_image=img.home_b_final, size=(25, 25))
+        self.lab = ctk.CTkImage(light_image=img.lab_p_final, dark_image=img.lab_b_final, size=(25, 25))
+        self.espiro = ctk.CTkImage(light_image=img.lung_p_final, dark_image=img.lung_b_final, size=(25, 25))
+        self.calc = ctk.CTkImage(light_image=img.calc_p_final, dark_image=img.calc_b_final, size=(25, 25))
+        self.config = ctk.CTkImage(light_image=img.config_p_final, dark_image=img.config_b_final, size=(25, 25))
+        self.help = ctk.CTkImage(light_image=img.ajuda_p_final, dark_image=img.ajuda_b_final, size=(25, 25))                  
         
         # Barra lateral
 
@@ -338,7 +323,7 @@ Insira seus exames no campo abaixo')
         self.scaling_optionmenu.set("100%")
 
         # Tela inicial padrão
-        self.select_frame("lab")
+        self.select_frame("home")
 
     def select_frame(self, frame):
         # Cor do botão
